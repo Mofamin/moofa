a7b769119afff455ab4a3055e8dc078a961b9559
热带雨林 <1143983754@qq.com>
2020-11-29 00:58:58 +0800

    1.增加三层结构 2.测试Mybatis连接mysql8成功

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
+
+    @Resource
+    private TestService testService;
+
+
     @GetMapping("/test")
-    public String test() {
-        return "Success";
+    public List<Test> test() {
+        return testService.list();
     }
 }
---
+++ system/src/main/java/com/course/system/mapper/TestMapper.java
@@ -0,0 +1,13 @@
+package com.course.system.mapper;
+
+import com.course.system.domain.Test;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+//@Repository
+//@Mapper
+public interface TestMapper {
+    public List<Test> list();
+}
