efc6fd88e2bf3aade525d6020a0ddd2c0ef10558
热带雨林 <1143983754@qq.com>
2020-11-30 23:48:14 +0800

    1.新建公共模块server

---
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -0,0 +1,30 @@
+package com.course.server.domain;
+
+public class Test {
+    private Integer id;
+    private String value;
+
+    public Test() {
+    }
+
+    public Test(Integer id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
